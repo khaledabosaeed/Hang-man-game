c52c79691d1904f58d55290e8a03a313be04be4e
khaledabosaeed <kahled242@gmail.com>
2025-04-24 12:10:58 +0300

    full-project

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 // Letters
-const letters = "abcdefghijklmnopqrstvwxyz";
+const letters = "abcdefghijklmnopqrstuvwxyz";
 const word = {
-  programming: ["java", "PHP", "react", "JavaScript", "HTML"],
-  cities: ["Gaza", "Juraslem"],
-  pepole: ["Fawzi", "khaled", "moamen"],
+  programming: [
+    "php",
+    "javascript",
+    "go",
+    "scala",
+    "fortran",
+    "mysql",
+    "python",
+  ],
+  movies: [
+    "Prestige",
+    "Inception",
+    "Parasite",
+    "Interstellar",
+    "Whiplash",
+    "Memento",
+    "Coco",
+    "Up",
+  ],
+  people: [
+    "Albert Einstein",
+    "Hitchcock",
+    "Alexander",
+    "Cleopatra",
+    "Mahatma Ghandi",
+  ],
+  countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"],
 };
 
 // get random category
@@ -20,7 +44,6 @@ let randomValue = Math.floor(Math.random() * word[randomName].length);
 let guesWord = word[randomName][randomValue];
 console.log(guesWord);
 
-
 // create Array from letters
 const lettersArray = Array.from(letters);
 let lettersContinar = document.querySelector(".letters");
@@ -36,19 +59,68 @@ lettersArray.forEach((element) => {
   lettersContinar.appendChild(span);
 });
 
-// add input to guess Word to the body 
-let gussedLetter = Array.from(guesWord);
-let gussContinar= document.querySelector(".letter-guess")
+// add input to guess Word to the body
+let gussedLetter = Array.from(guesWord.toLowerCase());
+console.log(gussedLetter);
+// All span we have
+
+let gussContinar = document.querySelector(".letter-guess");
 gussedLetter.forEach((letter) => {
   let EmptySpan = document.createElement("span");
-    if (letter ===' ') {
-        EmptySpan.classList.add("Empty")
-    }
-    gussContinar.appendChild(EmptySpan);
+  if (letter === " ") {
+    EmptySpan.classList.add("Empty");
+  }
+  gussContinar.appendChild(EmptySpan);
 });
 
-document.addEventListener("click",(e)=>{
-        if (e.target.className==="letter-box") {
-            e.target.classList.add("clicked")
-        }
-})
+let guessedSpan = document.querySelectorAll(".letter-guess span");
+
+let attmpet = 1;
+document.addEventListener("click", (e) => {
+  let stutes = false;
+  // check if click on key
+  if (e.target.className === "letter-box") {
+    // add class clicked to the clicked key
+    e.target.classList.add("clicked");
+    // get the letter
+    let currntLetter = e.target.innerHTML;
+
+    // check if the letter is in the gussedLetter
+    gussedLetter.forEach((letter, index) => {
+      if (letter === currntLetter) {
+        // add the letter to the guessedSpan
+        stutes = true;
+        guessedSpan.forEach((span, indexSpan) => {
+          if (index === indexSpan) {
+            span.innerHTML = letter;
+            console.log(`found in index ${index}`);
+          }
+        });
+      }
+    });
+  }
+  if (stutes != true) {
+    // add class wrong to the clicked key
+    document.querySelector(".man-draw").classList.add(`wrong-${attmpet}`);
+    attmpet++;
+    document.getElementById("failed").play();
+    if (attmpet == 8) {
+      document.querySelector(".letters").classList.add("disabled");
+      gameOver();
+    }
+  }
+  if (stutes == true) {
+    document.getElementById("scusess").play();
+  }
+});
+function gameOver() {
+  let gameOver = document.createElement("div");
+  gameOver.classList.add("game-over");
+  gameOver.innerHTML = `
+  <div class="game-over-content">
+    <h2>Game Over</h2>
+    <p>You lost the game</p>
+  </div>  
+  `;
+  document.body.appendChild(gameOver);
+}
