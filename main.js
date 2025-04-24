7cba0468414f41073c0e589b7609c94be692616a
khaledabosaeed <kahled242@gmail.com>
2025-04-24 10:05:44 +0300

    tha mean and the logic

--- main.js
+++ main.js
@@ -5,25 +5,27 @@ const word = {
   cities: ["Gaza", "Juraslem"],
   pepole: ["Fawzi", "khaled", "moamen"],
 };
-//
+
 // get random category
 let allkey = Object.keys(word);
 let randomKey = Math.floor(Math.random() * allkey.length);
 let randomName = allkey[randomKey];
 
 // set the category name
-let category = document.querySelector(".category");
-category.innerHTML = ` word from :<span>${randomName}</span>`;
+document.querySelector(".category span").innerHTML = randomName;
 
 // get random value
 let randomValue = Math.floor(Math.random() * word[randomName].length);
-console.log(word[randomName][randomValue]);
-    
 
-// console.log(randomKey);
+let guesWord = word[randomName][randomValue];
+console.log(guesWord);
+
+
+// create Array from letters
 const lettersArray = Array.from(letters);
-// console.log(lettersArray);
 let lettersContinar = document.querySelector(".letters");
+
+// add to page
 lettersArray.forEach((element) => {
   // crrate span
   let span = document.createElement("span");
@@ -33,3 +35,20 @@ lettersArray.forEach((element) => {
   span.className = "letter-box";
   lettersContinar.appendChild(span);
 });
+
+// add input to guess Word to the body 
+let gussedLetter = Array.from(guesWord);
+let gussContinar= document.querySelector(".letter-guess")
+gussedLetter.forEach((letter) => {
+  let EmptySpan = document.createElement("span");
+    if (letter ===' ') {
+        EmptySpan.classList.add("Empty")
+    }
+    gussContinar.appendChild(EmptySpan);
+});
+
+document.addEventListener("click",(e)=>{
+        if (e.target.className==="letter-box") {
+            e.target.classList.add("clicked")
+        }
+})
